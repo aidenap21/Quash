2cba5f378dc4f9b02bfafeab72d3c3c94e15ca41
Aiden Patel <aiden.patel21@gmail.com>
2023-10-20 21:00:32 -0500

    added comments as a template for functions

--- quash.c
+++ quash.c
@@ -4,57 +4,101 @@
 #include <strings.h>
 #include <errno.h>
 
+#define BSIZE 256
+
+//these are bash specified bins
+//maybe make different file for each function and run it instead of in 1 big file?
+//shouldn't use bash directories and should build our own instead I think
+#define BASH_EXEC  "/bin/bash"
+#define FIND_EXEC  "/bin/find"
+#define XARGS_EXEC "/usr/bin/xargs"
+#define GREP_EXEC  "/bin/grep"
+#define SORT_EXEC  "/bin/sort"
+#define HEAD_EXEC  "/usr/bin/head"
+
+
 
 // List Directory Contents - ls
 int ls(char arg) { // takes in single letter argument
-
+    //need to find list of arguments it takes and implement each one
 }
 
-// Foreground Executables
-int forexe(char *exe, char *arg) { // takes in executable name and arguments as a string
+// Foreground Executables (In Progress)
+int forExe(char *exe, char *arg) { // takes in executable name and arguments as a string
+    int status;
     pid_t p = fork();
-    if(p == 0) {
 
+    if(p == 0) {
+        char cmdbuf[BSIZE];
+        bzero(cmdbuf, BSIZE);
+        sprintf(cmdbuf, "%s %s", exe, arg);
+
+        //exec call needs to change since BASH_EXEC isn't going to be there
+        if ((execl(BASH_EXEC, BASH_EXEC, "-c", cmdbuf, (char *) 0)) < 0) {
+            fprintf(stderr, "\nError execing bash. ERROR#%d\n", errno);
+            return EXIT_FAILURE;
+        }
     }
+
+    if ((waitpid(p, &status, 0)) == -1) {
+        fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
+        return EXIT_FAILURE;
+    }
+
+    return 0;
 }
 
 // Background Executables - &
-int backexe(char *exe, char *arg) { // takes in executable name and arguments as a string
-
+int backExe(char *exe, char *arg) { // takes in executable name and arguments as a string
+    //should be similar to forExe but need to hide the process in the background
+    //maybe pipes that don't wait for it to return so that other things can happen?
 }
 
 
-// Print String - echo
+// Print String - echo (In Progress)
 int echoString(char *string) { // takes in string to print
-
+    // maybe need different method than direct printing?
+    printf("%s", string); 
 }
 
 // Set Value of Environmental Variable - export
 int export(char *var, char *val) { // takes in variable name and value to update with (not positive about variable type)
-
+    //need to know what variables need to actually be defined that are going to be updated
+    //check var and compare to variable names to figure out what to update
 }
 
 // Change Working Directory - cd
 int changeDir(char *path) { // takes in new path to navigate to
-
+    //should be simple function call
 }
 
 // Print Path of Current Directory - pwd
 int workingDir() {
-
+    //should also be simple function call
 }
 
 // Print All Running Background Process - jobs
 int printJobs() {
+    //need an array that is storing all currently running jobs
+    //iterate through the job array and print
 
 }
 
 // Send POSIX Signal to Process - kill
 int sendSig(int signal, int pid) { // takes in POSIX signal and PID to send signal to
-
+    //need to figure out how to force signal being sent since the user isn't actually pressing the command
+    //also need to ensure that the signal doesn't force the quash program itself to close
 }
 
 int parser(char *input) { // takes in string to parse
+    //start at front of string and iterate through and store each character in a new buffer
+    //stop when hitting one of the special characters or end of line
+    //if stopped from end of line then check if any of the commands that don't take in any parameters
+    //if it doesn't match any of the commands then call forExe on it to run it as an executable
+    //if stopped from special character then wait till finished parsing
+    //continue reading if more characters after special character and store the second half in different buffer
+    //run error message if not enough or too many parameters are given instead of passing in function and finding an error
+    //each function should have independent error handling if incorrect parameters are passed, parser should just check quantity and type
 
 }
 // Input Redirection - < (Build into parser)
@@ -70,5 +114,10 @@ int parser(char *input) { // takes in string to parse
 // Pipes and Redirects Work with Built//In Commands
 
 int main() {
-
+    printf("Welcome...\n");
+    while(1) {
+        //waits for user input at the start of each loop
+        //takes the input and passes it to parser which utilizes it from there
+        //error handling in here?
+    }
 }
